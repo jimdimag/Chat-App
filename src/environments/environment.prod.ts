162fc536600a50dce89a74d3e57b9e0ba6165236
Jim DiMaggio <jimdimag@gmail.com>
2019-06-19 09:52:59 -0400

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDpn4DHCTTPfx674u6cl3fIPozv_0gWIlE',
+    authDomain: 'chatapp-31a22.firebaseapp.com',
+    databaseURL: 'https://chatapp-31a22.firebaseio.com',
+    projectId: 'chatapp-31a22',
+    storageBucket: '',
+    messagingSenderId: '958106280673',
+    appId: '1:958106280673:web:7f2f0c3a0ec4fe48'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDpn4DHCTTPfx674u6cl3fIPozv_0gWIlE',
+    authDomain: 'chatapp-31a22.firebaseapp.com',
+    databaseURL: 'https://chatapp-31a22.firebaseio.com',
+    projectId: 'chatapp-31a22',
+    storageBucket: '',
+    messagingSenderId: '958106280673',
+    appId: '1:958106280673:web:7f2f0c3a0ec4fe48'
+  }
 };
 
 /*
